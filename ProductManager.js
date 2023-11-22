c0bb9533b42dc9bea66078de0c25e2bd27e17397
tazmin22 <sacaraat@gmail.com>
2023-11-21 23:24:10 -0300

    ultimos

--- ProductManager.js
+++ ProductManager.js
@@ -64,9 +64,16 @@ module.exports = { PManager: productManager }
 
 async function cosasAsincronicas() {
   const usuariosManager = new productManager("pruebas.json");
-  //await usuariosManager.addProducts("title", "description", "price", "thumbnail", "code", "stock");
-  //await usuariosManager.addProducts("title", "description", "price", "thumbnail", "code", "stock");
- // await usuariosManager.addProducts("producto", "una buena descripcion", 123, "sin imagen", 3, 3);
+  //await usuariosManager.addProducts("producto 1", "description", "price", "thumbnail", "code", "stock");
+  //await usuariosManager.addProducts("producto 2", "description", "price", "thumbnail", "code", "stock");
+  //await usuariosManager.addProducts("producto 3", "una buena descripcion", 123, "sin imagen", 3, 3);
+  //await usuariosManager.addProducts("producto 4", "description", "price", "thumbnail", "code", "stock");
+ // await usuariosManager.addProducts("producto 5", "description", "price", "thumbnail", "code", "stock");
+ // await usuariosManager.addProducts("producto 6", "una buena descripcion", 123, "sin imagen", 3, 3);
+  //await usuariosManager.addProducts("producto 7", "description", "price", "thumbnail", "code", "stock");
+ // await usuariosManager.addProducts("producto 8", "description", "price", "thumbnail", "code", "stock");
+  //await usuariosManager.addProducts("producto 9", "una buena descripcion", 123, "sin imagen", 3, 3);
+ //await usuariosManager.addProducts("producto 9", "una buena descripcion", 123, "sin imagen", 3, 3);
   //await usuariosManager.deleteProduct(3);
 
   //const producto = await usuariosManager.updateProduct(1, "title", "probando un nuevo titulo"  );
--- esto.js
+++ esto.js
@@ -4,23 +4,31 @@ const app = express()
 const port = 8080
 
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 const productManager = new PManager ("./pruebas.json")
 
-app.get("/products", async  (req, res) => {
-    let devolver = req.query;
-    const products = await productManager.getProducts();
+app.get("/products", async (req, res) => {
+  let devolver = req.query;
+  let products = await productManager.getProducts();
+  const limit = parseInt(req.query.limit, 10);
+
+  if (limit > 0) {
+    products = products.slice(0, limit);
+  }
+
+  res.send({ products });
+});
+
+ 
+  app.get("/products/:id",   (req, res) => {
+    
+    const id = req.params.id;
+    const products =  productManager.getProductsById(id);
+    
     res.send({products});
   });
-
-  //app.get("/products/:id", async  (req, res) => {
-    
-    //const id = req.params.id;
-    //const encontrados = products.find((item) => item.id === id);
-    //res.send({encontrados});
-  //});
     
 
   app.listen(port, () => {
